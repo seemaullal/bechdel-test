fb7114d589d26cc1f16926249a88fbcbfb9d914a
Seema <seemaullal@gmail.com>
2015-03-12 12:41:02 -0400

    deleted unecessary files

--- public/js/directives/groupselector/groupselector.js
+++
@@ -1,7 +0,0 @@
-app.directive("groupselector", function(){
-	console.log("groupselector is getting called right now")
-	return {
-		restrict: 'E',
-		templateUrl: 'js/directives/groupselector/groupselector.html'
-	};
-});
