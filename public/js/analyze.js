60e87f57e4e40014dffa9ba3b6e58cbb3a9b0719
Sarah Muenzinger <sarah.muenzinger@gmail.com>
2015-03-13 10:12:38 -0400

    more nodes

--- public/js/analyze.js
+++ public/js/analyze.js
@@ -1,10 +1,151 @@
+var commonWords = [
+  "the",
+  "be",
+  "to",
+  "of",
+  "and",
+  "a",
+  "in",
+  "that",
+  "have",
+  "I",
+  "it",
+  "for",
+  "not",
+  "on",
+  "with",
+  "he",
+  "as",
+  "you",
+  "do",
+  "at",
+  "this",
+  "but",
+  "his",
+  "by",
+  "from",
+  "they",
+  "we",
+  "say",
+  "her",
+  "she",
+  "or",
+  "an",
+  "will",
+  "my",
+  "one",
+  "all",
+  "would",
+  "there",
+  "their",
+  "what",
+  "so",
+  "up",
+  "out",
+  "if",
+  "about",
+  "who",
+  "get",
+  "which",
+  "go",
+  "me",
+  "when",
+  "make",
+  "can",
+  "like",
+  "time",
+  "no",
+  "just",
+  "him",
+  "know",
+  "take",
+  "people",
+  "into",
+  "year",
+  "your",
+  "good",
+  "some",
+  "could",
+  "them",
+  "see",
+  "other",
+  "than",
+  "then",
+  "now",
+  "look",
+  "only",
+  "come",
+  "its",
+  "over",
+  "think",
+  "also",
+  "back",
+  "after",
+  "use",
+  "two",
+  "how",
+  "our",
+  "work",
+  "first",
+  "well",
+  "way",
+  "even",
+  "new",
+  "want",
+  "because",
+  "any",
+  "these",
+  "give",
+  "day",
+  "most",
+  "us",
+  "here",
+  "such",
+  "much",
+  "yet",
+  "very",
+  "every",
+  "many"
+];
+var linkFinder = function(script, topNames){
+	var nameCatcher = /\s[A-Z]+\s/g;
+	var names = script.match(nameCatcher);
+	names = names.filter(function(word){
+		word = word.replace(/(\r\n|\n|\r)/gm,"").trim();
+		return word.length > 1;
+	});
+	var lines = script.split(nameCatcher);
+	var conversations = [];
+	for(var nameIndex = 0; nameIndex < (names.length); nameIndex++){
+		var convo = {};
+		convo.personA = names[nameIndex];
+		convo.personB = names[nameIndex+1];
+		convo.line = lines[nameIndex];
+		conversations.push(convo);
+	}
+	console.log("names",names);
+	console.log("lines",lines);
+	console.log("conversations",conversations);
+	links = []
+	conversations.forEach(function(convo){
+		var source = topNames.indexOf(convo.personA);
+		var target = topNames.indexOf(convo.personB);
+		if (source > -1 && target > -1){
+			link = {"source": source, "target":target, "value":2}
+			links.push(link)
+		}
+	})
+	return links
+	//
+	//
+}
 var analyzer = function(script){
 	var nameCatcher = /\s[A-Z]+\s/g;
 	var names = script.match(nameCatcher);
 	var nameAndGenders = {};
 	names = names.filter(function(word){
 		word = word.replace(/(\r\n|\n|\r)/gm,"").trim();
-		return word.length > 1;
+		return word.length > 1 && (commonWords.indexOf(word.toLowerCase())== -1);
 	});
 	var counts = _.countBy(names);
 	console.log("counts", counts)
@@ -17,15 +158,16 @@ var analyzer = function(script){
 			})
 		.sortBy('count').value();
 	console.log(sorted);
-	var topTenNames = sorted.reverse().slice(0,10);
-	console.log(topTenNames);
+	var topNames = sorted.reverse().slice(0,50);
+	console.log(topNames);
 	var nodes = [];
-	topTenNames.forEach(function(nameObj, index, arr){
+	topNames.forEach(function(nameObj, index, arr){
 		nodes.push({"name":nameObj.name, "group":index});
 		arr[index] = nameObj.name;
 
 	})
-	 noder(nodes)
+	var links = linkFinder(script, topNames)
+	noder(nodes, links);
 	
 }
 $(document).ready(function(){
@@ -35,14 +177,14 @@ $(document).ready(function(){
     });
 
 });
-function noder(nodes){
+function noder(nodes, links){
 	console.log("nodes",nodes)
-	var width = 960,
-	height = 500;
+	var width = 1000,
+	height = 1000;
 	var color = d3.scale.category20();
 	var force = d3.layout.force()
 	.charge(-120)
-	.linkDistance(30)
+	.linkDistance(200)
 	.size([width, height]);
 	var svg = d3.select("body").append("svg")
 	.attr("width", width)
@@ -50,11 +192,11 @@ function noder(nodes){
 	d3.json("mis.json", function(error, graph) {
 	force
 	.nodes(nodes)
-	.links(graph.links)
+	.links(links)
 	.start();
 	
 	var link = svg.selectAll(".link")
-	.data(graph.links)
+	.data(links)
 	.enter().append("line")
 	.attr("class", "link")
 	.style("stroke-width", function(d) { return Math.sqrt(d.value); });
