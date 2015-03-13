78030d08a3ff173caa1488c86e102584f5e3d338
Seema <seemaullal@gmail.com>
2015-03-13 11:21:44 -0400

    backend changes

--- routes/index.js
+++ routes/index.js
@@ -10,80 +10,6 @@ router.get('/', function(req, res, next) {
 	res.sendFile('./index.html', {root: './'});
 });
 
-
-// router.get('/testcrawl', function(req, res) {
-// 	var startUrl = 'http://www.imsdb.com/all%20scripts/';
-
-// 	// URL variables
-// 	var visitedUrls = [], pendingUrls = [];
-
-// 	// Create instances
-// 	var casper = require('casper').create({ /*verbose: true, logLevel: 'debug'*/ });
-// 	var utils = require('utils')
-// 	var helpers = require('./helpers')
-
-// 	// Spider from the given URL
-// 	function spider(url) {
-
-// 		// Add the URL to the visited stack
-// 		visitedUrls.push(url);
-
-// 		// Open the URL
-// 		casper.open(url).then(function() {
-
-// 			// Set the status style based on server status code
-// 			var status = this.status().currentHTTPStatus;
-// 			switch(status) {
-// 				case 200: var statusStyle = { fg: 'green', bold: true }; break;
-// 				case 404: var statusStyle = { fg: 'red', bold: true }; break;
-// 				 default: var statusStyle = { fg: 'magenta', bold: true }; break;
-// 			}
-
-// 			// Display the spidered URL and status
-// 			this.echo(this.colorizer.format(status, statusStyle) + ' ' + url);
-
-// 			// Find links present on this page
-// 			var links = this.evaluate(function() {
-// 				var links = [];
-// 				Array.prototype.forEach.call(__utils__.findAll('a'), function(e) {
-// 					links.push(e.getAttribute('href'));
-// 				});
-// 				return links;
-// 			});
-
-// 			// Add newly found URLs to the stack
-// 			var baseUrl = this.getGlobal('location').origin;
-// 			Array.prototype.forEach.call(links, function(link) {
-// 				var newUrl = helpers.absoluteUri(baseUrl, link);
-// 				if (pendingUrls.indexOf(newUrl) == -1 && visitedUrls.indexOf(newUrl) == -1) {
-// 					//casper.echo(casper.colorizer.format('-> Pushed ' + newUrl + ' onto the stack', { fg: 'magenta' }));
-// 					pendingUrls.push(newUrl);
-// 				}
-// 			});
-
-// 			// If there are URLs to be processed
-// 			if (pendingUrls.length > 0) {
-// 				var nextUrl = pendingUrls.shift();
-// 				//this.echo(this.colorizer.format('<- Popped ' + nextUrl + ' from the stack', { fg: 'blue' }));
-// 				spider(nextUrl);
-// 			}
-
-// 		});
-
-// 	}
-
-// 	// Start spidering
-// 	casper.start(startUrl, function() {
-// 		spider(startUrl);
-// 	});
-
-// 	// Start the run
-// 	casper.run();
-
-// 	console.log(visitedUrls);
-// 	res.send(visitedUrls);
-
-// });
 router.get('/testcrawl', function(req, res) {
 
 	var crawler = new Crawler().configure({
@@ -101,6 +27,33 @@ router.get('/testcrawl', function(req, res) {
 	res.send(urls);
 });
 
+router.get('/api/getcast/:moviename', function(req,res) {
+	var url = 'http://api.rottentomatoes.com/api/public/v1.0/movies.json';
+	var qs = {
+		apikey : 'tg7n4r3efe9dezvrpk3t6j7m',
+		q : req.params.moviename,
+		page_limit : 1
+	};
+	request({url: url, qs: qs }, function (error, response, body) {
+		if (!error && response.statusCode == 200) {
+			body = JSON.parse(body);
+			var id = body.movies[0].id;
+			castUrl = 'http://api.rottentomatoes.com/api/public/v1.0/movies/' + id + '/cast.json?apikey=tg7n4r3efe9dezvrpk3t6j7m';
+			request({url: castUrl, qs: qs }, function (err, response, body) {
+				body = JSON.parse(body);
+				var characters = [ ];
+				body.cast.forEach(function (char) {
+					characters.push(char.characters[0]);
+				});
+				res.send(characters);
+			});
+
+			// console.log(body);
+			// res.send(body);
+		}
+	});
+
+});
 
 module.exports = router;
 
