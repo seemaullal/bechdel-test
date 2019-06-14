db1bbdcdc1ee0173781e503b85829ccec3b3a592
Seema Ullal <seema.ullal@us-sullal3.localdomain>
2019-06-13 18:52:09 -0700

    Removed swig

--- app.js
+++ app.js
@@ -4,7 +4,6 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var session = require('express-session');
-var swig = require('swig');
 
 var bodyParser = require('body-parser');
 
